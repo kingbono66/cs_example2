0224b0da77710d90342459e0c3a6e1531ece1257
academy <Administrator@G-07>
2020-06-29 11:28:28 +0900

    .

---
+++ cs_example2/cs_example2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace cs_example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
