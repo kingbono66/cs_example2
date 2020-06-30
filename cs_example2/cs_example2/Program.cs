ad40fb866d49f2262dcdf97190ae8c2b55e8db31
academy <Administrator@G-07>
2020-06-30 12:43:59 +0900

    ..

--- cs_example2/cs_example2/Program.cs
+++ cs_example2/cs_example2/Program.cs
@@ -12,6 +12,42 @@ namespace cs_example2
     {
         static void Main(string[] args)
         {
+            Cat kitty = new Cat();
+            kitty.name = "키티";
+            kitty.color = "분홍색";
+            kitty.Meow();
+
+            Cat cat = new Cat();
+            cat.name = "네로";
+            cat.color = "검은색";
+            cat.Meow();
+
+            Cat unknown = new Cat();
+            unknown.Meow();
+
         }
+    }
+
+    class Cat
+    {
+        public string name;
+        public string color;
+
+        public Cat()
+        {
+            name = "나비";
+            color = "몰라";
+        }
+
+        public void Meow()
+        {
+            WriteLine($"{name}({color}) : 야옹");
+        }
+
+        ~Cat()
+        {
+            WriteLine($"{name} : bye");
+        }
+
     }
 }
