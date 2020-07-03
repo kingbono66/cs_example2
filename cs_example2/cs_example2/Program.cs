71811263f939404dfe7afca5621eb1f67da110ab
academy <Administrator@G-07>
2020-07-03 12:16:22 +0900

    .

--- cs_example2/cs_example2/Program.cs
+++ cs_example2/cs_example2/Program.cs
@@ -12,6 +12,50 @@ namespace cs_example2
     {
         static void Main(string[] args)
         {
+            Mammal mammal = new Dog();
+            Dog dog;
+
+            if(mammal is Dog)
+            {
+                dog = (Dog)mammal;
+                dog.Bark();
+            }
+
+            Mammal mammal2 = new Cat();
+            Cat cat = mammal2 as Cat;
+            if (cat != null)
+            {
+                cat.Meow();
+            }
+
+            Cat cat2 = mammal as Cat;
+            if (cat2 != null)
+                cat2.Meow();
+            else
+                WriteLine("cat2는 고양이가 아니야");
+
+        }
+
+        class Mammal
+        {
+            public void Nurse()
+            {
+                WriteLine("젖먹이");
+            }
+        }
+        class Dog : Mammal
+        {
+            public void Bark()
+            {
+                WriteLine("멍!");
+            }
+        }
+        class Cat : Mammal
+        {
+            public void Meow()
+            {
+                WriteLine("야옹~");
+            }
         }
     }
 }
