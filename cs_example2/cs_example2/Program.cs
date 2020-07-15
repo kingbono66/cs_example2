ae2c5a6f1d2f1a0e9dc50f3b47f29e1b1a5a0f0a
oh <kyunggioh@hanmail.net>
2020-07-15 11:48:45 +0900

    .

--- cs_example2/cs_example2/Program.cs
+++ cs_example2/cs_example2/Program.cs
@@ -12,6 +12,63 @@ namespace cs_example2
     {
         static void Main(string[] args)
         {
+            //Class c = new Class();
+            //c.A = 3;
+            //WriteLine(c.A);
+
+            BirthdayInfo birth = new BirthdayInfo()
+            {
+                Name = "서현",
+                Birthday = new DateTime(1991,6,28)
+            };
+            //WriteLine($"Name : {birth.Name}");
+            //WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            //WriteLine($"Age : {birth.Age}");
+
+            //birth.Name = "서현";
+            //birth.Birthday = new DateTime(1991,6,28);
+
+            WriteLine($"Name : {birth.Name}");
+            WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            WriteLine($"Age : {birth.Age}");
+
+        }
+
+
+    }
+
+    class Class
+    {
+        //private int a;
+        
+        /*
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+        */
+        public int A { get; set; }
+    }
+
+    class BirthdayInfo
+    {
+        public string Name { get; set; } = "Unknown";
+
+        public DateTime Birthday { get; set; } = new DateTime(1,1,1);
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
         }
     }
+
 }
