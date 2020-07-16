98dec35e085b762e68e37df3c0b2ec92176f83e6
oh <kyunggioh@hanmail.net>
2020-07-16 12:39:16 +0900

    .

--- cs_example2/cs_example2/Program.cs
+++ cs_example2/cs_example2/Program.cs
@@ -8,10 +8,86 @@ using static System.Console;
 
 namespace cs_example2
 {
+    delegate int Compare(int a, int b);
     class Program
     {
         static void Main(string[] args)
         {
+            {
+                int[] array = { 3, 7, 4, 2, 10 };
+
+                foreach (int i in array)
+                {
+                    Write($"{i} ");
+                }
+                WriteLine();
+
+                WriteLine("오름차순 정렬");
+                BubbleSort(array, new Compare(AscendCompare));
+
+                foreach (int i in array)
+                {
+                    Write($"{i} ");
+                }
+                WriteLine();
+            }
+            WriteLine();
+
+            {
+                int[] array = { 3, 7, 4, 2, 10 };
+
+                foreach (int i in array)
+                {
+                    Write($"{i} ");
+                }
+                WriteLine();
+
+                WriteLine("내림차순 정렬");
+                BubbleSort(array, new Compare(DescendCompare));
+
+                foreach (int i in array)
+                {
+                    Write($"{i} ");
+                }
+                WriteLine();
+            }
+        }
+
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+        static void BubbleSort(int[] dataSet, Compare Comparer)
+        {
+            int temp = 0;
+
+            for( int i = 0; i < dataSet.Length - 1; i++)
+            {
+                for( int j = 0; j <dataSet.Length - (i+1); j++)
+                {
+                    if(Comparer(dataSet[j],dataSet[j+1]) > 0)
+                    {
+                        temp = dataSet[j + 1];
+                        dataSet[j + 1] = dataSet[j];
+                        dataSet[j] = temp;
+                    }
+                }
+            }
         }
     }
+
 }
