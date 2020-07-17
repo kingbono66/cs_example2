4cd4a439fbeef54bba598054d535a7f7a855c6c5
oh <kyunggioh@hanmail.net>
2020-07-17 11:34:58 +0900

    .

--- cs_example2/cs_example2/Program.cs
+++ cs_example2/cs_example2/Program.cs
@@ -8,10 +8,27 @@ using static System.Console;
 
 namespace cs_example2
 {
+    delegate void ThereIsAFire(string location);
     class Program
     {
         static void Main(string[] args)
         {
+
+        }
+
+        static void Call119(string location)
+        {
+            WriteLine($"소방서죠? {location}에 불이 났어요.");
+        }
+        static void ShotOut(string location)
+        {
+            WriteLine($"피하세요 {location}에 불이 났어요.");
+        }
+        static void Escape(string location)
+        {
+            WriteLine($"{location}에서 도망갑시다!");
         }
     }
+
+    
 }
