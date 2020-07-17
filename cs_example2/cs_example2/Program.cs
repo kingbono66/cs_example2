57c525fb2bef9468297f227798669d8c961d5673
oh <kyunggioh@hanmail.net>
2020-07-17 12:34:21 +0900

    .

--- cs_example2/cs_example2/Program.cs
+++ cs_example2/cs_example2/Program.cs
@@ -8,10 +8,25 @@ using static System.Console;
 
 namespace cs_example2
 {
+    delegate void DoSomething();
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+            Action<int> Do = (i) =>
+            {
+                WriteLine("aaa");
+                WriteLine("bbb");
+                WriteLine($"{i}");
+            };
+            Do(1);
+            */
+
+            Func<int> f = () => 10;
+            WriteLine(f());
+            Func<int, int, string> f1 = (a, b) =>  (a + b).ToString();
+            WriteLine(f1(3,5));
         }
     }
 }
