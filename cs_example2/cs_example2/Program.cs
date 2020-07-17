efcfe04ae9b39ca127b5c7a1c89e61cda7b3ffb8
oh <kyunggioh@hanmail.net>
2020-07-17 11:51:29 +0900

    .

--- cs_example2/cs_example2/Program.cs
+++ cs_example2/cs_example2/Program.cs
@@ -13,13 +13,31 @@ namespace cs_example2
     {
         static void Main(string[] args)
         {
+            ThereIsAFire call119 = new ThereIsAFire(
+                delegate (string location)
+                {
+                    WriteLine($"소방서죠? {location}에 불이 났어요.");
+                });
+            call119 += new ThereIsAFire(ShotOut);
+            call119 += new ThereIsAFire(Escape);
+
+
+            call119("우리집");
+
+
+            /*
+            ThereIsAFire shotOut = new ThereIsAFire(ShotOut);
+            shotOut("우리집");
+            ThereIsAFire escape = new ThereIsAFire(Escape);
+            escape("우리집");
+            */
 
         }
 
-        static void Call119(string location)
-        {
-            WriteLine($"소방서죠? {location}에 불이 났어요.");
-        }
+        //static void Call119(string location)
+        //{
+        //    WriteLine($"소방서죠? {location}에 불이 났어요.");
+        //}
         static void ShotOut(string location)
         {
             WriteLine($"피하세요 {location}에 불이 났어요.");
